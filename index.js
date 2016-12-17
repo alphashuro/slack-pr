d22fefb6947b1850341112a6b1f0050461b2e57b
Alpha Shuro <alphashuro@gmail.com>
2016-12-18 01:51:55 +0200

    cleanup

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ const https = require('https')
 const port = process.env.PORT || 8080
 
 const slack = {
-    clientId: '56841000435.89670125541',
-    clientSecret: 'ad07397ffb0b8881f653c247392aee37',
-    sendMessage(text) {
-        const data = JSON.stringify({
-            channel: '@alpha',
-            text,
-        })
+    sendMessage(opt) {
+        const data = JSON.stringify(opt)
 
         const reqOptions = {
             hostname: 'hooks.slack.com',
@@ -48,14 +43,15 @@ const server = http.createServer((req, res) => {
             const { actor, pullrequest, repository } = body
 
             try {
-                const message = `@${users[actor.username] || actor.username} has made a PR '<${pullrequest.links.html.href}|${pullrequest.title}>'`
+                const message = `@${users[actor.username] || actor.username} has made a PR '<${pullrequest.links.html.href}|${pullrequest.title}>' `
                                 + `from <${pullrequest.source.repository.links.html.href}/branch/${pullrequest.source.branch.name}|${pullrequest.source.branch.name}> ` 
                                 + `into <${pullrequest.destination.repository.links.html.href}/branch/${pullrequest.destination.branch.name}|${pullrequest.destination.branch.name}> `
                                 + `on <${repository.links.html.href}|${repository.name}>`
 
-                console.log(message)
-                slack.sendMessage(message)
-
+                slack.sendMessage({
+                    channel: '@alpha',
+                    text: message,
+                }
             } catch (e) {
                 console.log(e)
             }
