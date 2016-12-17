2e61131cb7a0b69cc2a5791ca0dff51f6a4c671b
Alpha Shuro <alphashuro@gmail.com>
2016-12-18 00:37:04 +0200

    log post request body

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const http = require('http')
 const port = process.env.PORT || 8080
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/pr') {
-        console.log(req)
+    if (req.url === '/pr' && req.method === 'POST') {
+        let body = ''
+
+        req.on('data', data => body += data)
+        req.on('end', () => {
+            body = JSON.parse(body)
+
+            console.log(body)
+        })
+
         res.statusCode = 200
         res.end()
+        return;
     }
+
+    res.statusCode = 404
+    res.end()
 })
 
 server.listen(port)
